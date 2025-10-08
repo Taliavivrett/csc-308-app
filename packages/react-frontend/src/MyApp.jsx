e3d219a1f3f68ab568eef3631704ec00496e3ad1
Taliavivrett <taliavivrett@gmail.com>
2025-10-07 23:47:46 -0700

    feat: added return newly created object from post

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,19 +7,22 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function updateList(person) {
-    postUser(person)
-      .then((res) => {
-        if (res.status === 201) {
-          setCharacters([...characters, person]);
-        } else {
-          console.error("Failed to create user. Status:", res.status);
-        }
-      })
-      .catch((error) => {
-        console.error("Error posting user:", error);
-      });
-  }
+function updateList(person) {
+  postUser(person)
+    .then((res) => {
+      if (res.status === 201) {
+        return res.json();
+      } else {
+        throw new Error(`Failed to create user. Status: ${res.status}`);
+      }
+    })
+    .then((newUser) => {
+      setCharacters([...characters, newUser]);
+    })
+    .catch((error) => {
+      console.error("Error posting user:", error);
+    });
+}
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
